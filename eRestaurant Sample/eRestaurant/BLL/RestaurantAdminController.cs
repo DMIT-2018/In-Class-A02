a2156adaa62e3dc8c1d6095914f6b6dd0f5a08b0
Dan Gilleland <dgilleland@nait.ca>
2014-09-15 17:34:53 -0600

    BLL

---
+++ eRestaurant Sample/eRestaurant/BLL/RestaurantAdminController.cs
@@ -0,0 +1,90 @@
+﻿using eRestaurant.DAL;
+using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    public class RestaurantAdminController
+    {
+        #region Manage Waiters
+        #region Command
+        public int AddWaiter(Waiter item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                // TODO: Validation rules...
+                var added = context.Waiters.Add(item);
+                context.SaveChanges();
+                return added.WaiterID;
+            }
+        }
+
+        public void UpdateWaiter(Waiter item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                // TODO: Validation...
+                var attached = context.Waiters.Attach(item);
+                var existing = context.Entry<Waiter>(attached);
+                existing.State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteWaiter(Waiter item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                var existing = context.Waiters.Find(item.WaiterID);
+                context.Waiters.Remove(existing);
+                context.SaveChanges();
+            }
+        }
+        #endregion
+        #region Query
+        public List<Waiter> ListAllWaiters()
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                return context.Waiters.ToList();
+            }
+        }
+
+        public Waiter GetWaiter(int waiterId)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                return context.Waiters.Find(waiterId);
+            }
+        }
+        #endregion
+        #endregion
+
+        #region Manage Tables
+        #region Command
+        #endregion
+        #region Query
+        #endregion
+        #endregion
+
+        #region Manage Items
+        #region Command
+        #endregion
+        #region Query
+        #endregion
+        #endregion
+
+        #region Manage Special Events
+        #region Command
+        #endregion
+        #region Query
+        #endregion
+        #endregion
+
+
+    }
+}
