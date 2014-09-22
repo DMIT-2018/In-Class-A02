8e868f758926875f000b0d38b11c7165625becd0
Dan Gilleland <dgilleland@nait.ca>
2014-09-22 16:27:06 -0600

    BLL to manage Tables

--- eRestaurant Sample/eRestaurant/BLL/RestaurantAdminController.cs
+++ eRestaurant Sample/eRestaurant/BLL/RestaurantAdminController.cs
@@ -74,8 +74,61 @@ namespace eRestaurant.BLL
 
         #region Manage Tables
         #region Command
+        [DataObjectMethod(DataObjectMethodType.Insert, false)]
+        public int AddTable(Table item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                // TODO: Validation rules...
+                var added = context.Tables.Add(item);
+                context.SaveChanges();
+                return added.TableID;
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void UpdateTable(Table item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                // TODO: Validation...
+                var attached = context.Tables.Attach(item);
+                var existing = context.Entry<Table>(attached);
+                existing.State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void DeleteTable(Table item)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                var existing = context.Tables.Find(item.TableID);
+                context.Tables.Remove(existing);
+                context.SaveChanges();
+            }
+        }
         #endregion
+
         #region Query
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Table> ListAllTables()
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                return context.Tables.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Table GetTable(int TableId)
+        {
+            using (RestaurantContext context = new RestaurantContext())
+            {
+                return context.Tables.Find(TableId);
+            }
+        }
         #endregion
         #endregion
 
