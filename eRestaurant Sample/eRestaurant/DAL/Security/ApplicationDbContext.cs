9fe243849122bb057bee41e9e63e00585d799377
Dan Gilleland <dgilleland@nait.ca>
2014-11-14 09:23:49 -0700

    Moved ASP.NET Identity to Class Library

---
+++ eRestaurant Sample/eRestaurant/DAL/Security/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using eRestaurant.Entities.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.DAL.Security
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+
+}
