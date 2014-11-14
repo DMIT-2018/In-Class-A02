45fc069fd15b4d13730f6e54222c5b6b927cb49e
Dan Gilleland <dgilleland@nait.ca>
2014-11-14 09:25:58 -0700

    Added RoleManager to BLL

---
+++ eRestaurant Sample/eRestaurant/BLL/Security/RoleManager.cs
@@ -0,0 +1,19 @@
+﻿using eRestaurant.DAL.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL.Security
+{
+    public class RoleManager : RoleManager<IdentityRole>
+    {
+        public RoleManager()
+            : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
