092248baee959ccb116515dd55010c2a860c309e
Dan Gilleland <dgilleland@nait.ca>
2014-12-08 16:39:01 -0700

    Spur of the moment logic to split a bill

--- eRestaurant Sample/eRestaurant/BLL/WaiterController.cs
+++ eRestaurant Sample/eRestaurant/BLL/WaiterController.cs
@@ -1,4 +1,5 @@
 ﻿using eRestaurant.DAL;
+using eRestaurant.Entities;
 using eRestaurant.Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -53,7 +54,62 @@ namespace eRestaurant.BLL
         public void SplitBill(int billId, List<OrderItem> originalItems, List<OrderItem> newItems)
         {
             // TODO: Actually go through and split that bill into two
-            throw new NotImplementedException();
+            using (var context = new RestaurantContext())
+            {
+                // TODO: 0) Validation :)
+                // 1) Get the bill
+                var bill = context.Bills.Find(billId);
+                if (bill == null) throw new ArgumentException("Invalid Bill ID - does not exist");
+
+                // 2) Loop through bill items, if item not in original, remove
+                List<BillItem> toMove = new List<BillItem>();
+                foreach(var item in bill.Items)
+                {
+                    bool inOriginal = originalItems.Any(x => x.ItemName == item.Item.Description);
+                    bool inNewItems = newItems.Any(x => x.ItemName == item.Item.Description);
+                    if(!inOriginal)
+                    {
+                        // TODO: clean
+                        if (!inNewItems)
+                            throw new Exception("Hey - someone's got to pay for that!");
+                        toMove.Add(item);
+                    }
+                }
+                foreach (var item in toMove)
+                    context.BillItems.Remove(item);
+
+                // 3) Make a new bill
+                var newBill = new Bill()
+                {
+                    BillDate = bill.BillDate,
+                    Comment = "Split from bill# " + bill.BillID,
+                    NumberInParty = bill.NumberInParty, // meh 
+                    OrderPlaced = bill.OrderPlaced,
+                    OrderReady = bill.OrderReady,
+                    OrderServed = bill.OrderServed,
+                    WaiterID = bill.WaiterID
+                    // TODO: thorny question about rules around splitting bill for a single table vs. reservation
+                };
+
+                // 4) Add the new missing items to the new bill
+                foreach(var item in toMove)
+                {
+                    newBill.Items.Add(new BillItem()
+                        {
+                            ItemID = item.ItemID,
+                            Notes = item.Notes,
+                            Quantity = item.Quantity,
+                            SalePrice = item.SalePrice,
+                            UnitCost = item.UnitCost                            
+                        });
+                }
+
+                // 5) Add the new bill to the context
+                context.Bills.Add(newBill);
+
+                // 6) hope for the best.
+                context.SaveChanges();
+            }
         }
     }
 }
