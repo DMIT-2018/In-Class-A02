6f389ef34335482cc9fd76dca5467d88960bc0ad
Dan Gilleland <dgilleland@nait.ca>
2014-12-11 10:39:50 -0700

    Ad-hoc demo bulk processing

---
+++ eRestaurant Sample/eRestaurant/BLL/FauxLabController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    public class FauxLabController
+    {
+        public void UpdateExistingOrder(string placeholder)
+        {
+            // Validation
+            // PurchaseOrderDetails – Insert/Update/Delete
+            //   values as appropriate
+        }
+    }
+}
